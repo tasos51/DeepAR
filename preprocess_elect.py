7107e1f7f65295264099d84661d2013e35f3fcf0
Tasos <tasos1109@gmail.com>
2021-05-27 20:03:22 +0200

    Preprocessing script is now more generic
    It doesn't crash when is used with other datasets rather than this of electricity.

--- preprocess_elect.py
+++ preprocess_elect.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
 import os
 from datetime import datetime, timedelta
 import pandas as pd
@@ -22,24 +23,47 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def prep_data(data, covariates, data_start, train = True):
-    #print("train: ", train)
+def win_start_end(ith, stride_size, data_start, train):
+    # This function calculates the start and end possition of the ith window
+    # based on the number i, the stride size, and the data start index 
+    if train:
+        window_start = data_start + (stride_size * ith)
+    else:
+        window_start = stride_size * ith
+    window_end = window_start + window_size
+    
+    return window_start, window_end
+
+def calculate_windows(data, data_start, window_size, stride_size, train):
+    # This function calculates the number of windows per series (different method for train and test)
+    # based on the starting index of the data, the window size, the stride size 
+    # and whether the train variable is true or false
+    # needs more explanation of how is it working
     time_len = data.shape[0]
-    #print("time_len: ", time_len)
-    input_size = window_size-stride_size
-    windows_per_series = np.full((num_series), (time_len-input_size) // stride_size)
-    #print("windows pre: ", windows_per_series.shape)
-    if train: windows_per_series -= (data_start+stride_size-1) // stride_size
-    #print("data_start: ", data_start.shape)
-    #print(data_start)
-    #print("windows: ", windows_per_series.shape)
-    #print(windows_per_series)
+    input_size = window_size - stride_size
+    windows_per_series = np.full((num_series), (time_len - input_size) // stride_size)
+    if train: windows_per_series -= (data_start + stride_size - 1) // stride_size
+        
+    return windows_per_series
+
+def prep_data(data, covariates, data_start, train = True):
+    # Calculate the number of windows per series (different method for train and test)
+    windows_per_series = calculate_windows(data, data_start, window_size, stride_size, train)
+    # Calculate the total number of windows being used
     total_windows = np.sum(windows_per_series)
+    
+    # Calculate some lengths and other variables
+    time_len = data.shape[0]
+    input_size = window_size - stride_size
+    
+    # Init results arrays 
     x_input = np.zeros((total_windows, window_size, 1 + num_covariates + 1), dtype='float32')
     label = np.zeros((total_windows, window_size), dtype='float32')
     v_input = np.zeros((total_windows, 2), dtype='float32')
     #cov = 3: ground truth + age + day_of_week + hour_of_day + num_series
     #cov = 4: ground truth + age + day_of_week + hour_of_day + month_of_year + num_series
+    
+    # Calculate age covariates per series and fill in the arrays with the data + covariates
     count = 0
     if not train:
         covariates = covariates[-time_len:]
@@ -50,18 +74,9 @@ def prep_data(data, covariates, data_start, train = True):
         else:
             covariates[:, 0] = cov_age[-time_len:]
         for i in range(windows_per_series[series]):
-            if train:
-                window_start = stride_size*i+data_start[series]
-            else:
-                window_start = stride_size*i
-            window_end = window_start+window_size
-            '''
-            print("x: ", x_input[count, 1:, 0].shape)
-            print("window start: ", window_start)
-            print("window end: ", window_end)
-            print("data: ", data.shape)
-            print("d: ", data[window_start:window_end-1, series].shape)
-            '''
+            # Calculate the start and end position of the corresponding window
+            window_start, window_end = win_start_end(i, stride_size, data_start[series], train)
+    
             x_input[count, 1:, 0] = data[window_start:window_end-1, series]
             x_input[count, :, 1:1+num_covariates] = covariates[window_start:window_end, :]
             x_input[count, :, -1] = series
@@ -75,20 +90,21 @@ def prep_data(data, covariates, data_start, train = True):
                 if train:
                     label[count, :] = label[count, :]/v_input[count, 0]
             count += 1
+    # Prepare the path and save the data to .npy files
     prefix = os.path.join(save_path, 'train_' if train else 'test_')
-    np.save(prefix+'data_'+save_name, x_input)
-    np.save(prefix+'v_'+save_name, v_input)
-    np.save(prefix+'label_'+save_name, label)
+    np.save(prefix+'data_'+save_dir, x_input)
+    np.save(prefix+'v_'+save_dir, v_input)
+    np.save(prefix+'label_'+save_dir, label)
 
 def gen_covariates(times, num_covariates):
     covariates = np.zeros((times.shape[0], num_covariates))
     for i, input_time in enumerate(times):
         covariates[i, 1] = input_time.weekday()
-        covariates[i, 2] = input_time.hour
+        covariates[i, 2] = input_time.year
         covariates[i, 3] = input_time.month
     for i in range(1,num_covariates):
         covariates[:,i] = stats.zscore(covariates[:,i])
-    return covariates[:, :num_covariates]
+    return covariates
 
 def visualize(data, week_start):
     x = np.arange(window_size)
@@ -100,36 +116,31 @@ def visualize(data, week_start):
 if __name__ == '__main__':
 
     global save_path
-    name = 'LD2011_2014.txt'
-    save_name = 'elect'
-    window_size = 192
-    stride_size = 24
+    name = 'katametriseis_10_processed.csv'
+    save_dir = 'elect'
+    window_size = 5
+    stride_size = 1
     num_covariates = 4
-    train_start = '2011-01-01 00:00:00'
-    train_end = '2014-08-31 23:00:00'
-    test_start = '2014-08-25 00:00:00' #need additional 7 days as given info
-    test_end = '2014-09-07 23:00:00'
-    pred_days = 7
-    given_days = 7
+    train_start = '2009-01-31'
+    train_end = '2013-10-31'
+    test_start = '2014-01-31' #need additional 7 days as given info
+    test_end = '2018-10-31'
 
-    save_path = os.path.join('data', save_name)
+    save_path = os.path.join('data', save_dir)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     csv_path = os.path.join(save_path, name)
     if not os.path.exists(csv_path):
-        zipurl = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00321/LD2011_2014.txt.zip'
-        with urlopen(zipurl) as zipresp:
-            with ZipFile(BytesIO(zipresp.read())) as zfile:
-                zfile.extractall(save_path)
+        print('Please provide a valid csv file or path inside the save directory!')
+        sys.exit()
 
-    data_frame = pd.read_csv(csv_path, sep=";", index_col=0, parse_dates=True, decimal=',')
-    data_frame = data_frame.resample('1H',label = 'left',closed = 'right').sum()[train_start:test_end]
-    data_frame.fillna(0, inplace=True)
+
+    data_frame = pd.read_csv(csv_path, sep=",", index_col=0, parse_dates=True, decimal='.')
     covariates = gen_covariates(data_frame[train_start:test_end].index, num_covariates)
     train_data = data_frame[train_start:train_end].values
     test_data = data_frame[test_start:test_end].values
-    data_start = (train_data!=0).argmax(axis=0) #find first nonzero value in each time series
-    total_time = data_frame.shape[0] #32304
-    num_series = data_frame.shape[1] #370
+    data_start = (train_data!=0).argmax(axis=0) # find the first nonzero value in each time series
+    total_time = data_frame.shape[0] # number of time point
+    num_series = data_frame.shape[1] # number of series
     prep_data(train_data, covariates, data_start)
     prep_data(test_data, covariates, data_start, train=False)
